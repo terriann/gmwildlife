d6180e6da2115382c73f1f012d61459c00fa1c28
Terri Ann <terri.ann.s@gmail.com>
2020-10-12 17:12:32 -0400

    Updates

---
+++ precache-manifest.eceaa7ff91aafa5c25e9478870e63025.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6802f99ff14e7d50b66f2ecee36114f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc478acebe8eb99727b6",
+    "url": "/static/css/main.f3201de8.chunk.css"
+  },
+  {
+    "revision": "ea51bd727adfb2700fe4",
+    "url": "/static/js/2.c7625b2d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.c7625b2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc478acebe8eb99727b6",
+    "url": "/static/js/main.480c6c7b.chunk.js"
+  },
+  {
+    "revision": "32ed74bb70e459efe80a",
+    "url": "/static/js/runtime-main.4267953d.js"
+  },
+  {
+    "revision": "0ea1006263fd223bf2febea87d137a80",
+    "url": "/static/media/coyote-4JxlPhF33F8.0ea10062.jpg"
+  },
+  {
+    "revision": "b52f620ba55a984ef40bf0610473e376",
+    "url": "/static/media/racoon-aOnkavapVm4-unsplash.b52f620b.jpg"
+  },
+  {
+    "revision": "78734498503f1500295ff8f775f3d1b8",
+    "url": "/static/media/skunk-YoEA2dcDo0g-unsplash.78734498.jpg"
+  },
+  {
+    "revision": "651d7678d4158b4791e063aa62010993",
+    "url": "/static/media/winterbarn-1313903-unsplash.651d7678.jpg"
+  }
+]);
